b326a75ba28f51689459a64ed71bd45edc2e214f
Michaël Zasso <targos@protonmail.com>
2018-08-11 18:19:08 +0200

    feat: refactor to have a ES module build and use TypeScript (#3)
    Fixes: #2
    
    BREAKING CHANGE:
    
    The initialization now throws if the seed is not an integer.
    Stop supporting Node 4.

---
+++ src/__tests__/xsadd.test.ts
@@ -0,0 +1,104 @@
+import XSadd from '../xsadd';
+
+const resultUint32 = [
+  1823491521,
+  1658333335,
+  1467485721,
+  45623648,
+  3336175492,
+  2561136018,
+  181953608,
+  768231638,
+  3747468990,
+  633754442,
+  1317015417,
+  2329323117,
+  688642499,
+  1053686614,
+  1029905208,
+  3711673957,
+  2701869769,
+  695757698,
+  3819984643,
+  1221024953
+];
+
+const resultFloat = [
+  0.03331006,
+  0.81703216,
+  0.52816284,
+  0.76104426,
+  0.09285593,
+  0.34277064,
+  0.40109116,
+  0.79619718,
+  0.71619844,
+  0.21930778,
+  0.68432468,
+  0.05859083,
+  0.86583626,
+  0.22139138,
+  0.95107985,
+  0.15838248,
+  0.02087647,
+  0.60301602,
+  0.01047146,
+  0.34406352
+];
+
+describe('check return values', () => {
+  const xsadd = new XSadd(1234);
+
+  it('uint32', () => {
+    const result = [];
+    for (let i = 0; i < 40; i++) {
+      const val = xsadd.getUint32();
+      if (i < 20) {
+        result.push(val);
+      }
+    }
+    expect(result).toEqual(resultUint32);
+  });
+
+  it('float', () => {
+    const result = [];
+    for (let i = 0; i < 20; i++) {
+      result.push(xsadd.getFloat());
+    }
+    result.forEach((value, i) => {
+      expect(value).toBeCloseTo(resultFloat[i], 1e-6);
+    });
+  });
+});
+
+it('should be reinintiable', () => {
+  const xsadd = new XSadd(1234);
+  const val1 = xsadd.getUint32();
+  const val2 = xsadd.getFloat();
+
+  xsadd.init(1234);
+  expect(xsadd.getUint32()).toBe(val1);
+  expect(xsadd.getFloat()).toBe(val2);
+
+  xsadd.init(42);
+  expect(xsadd.getUint32()).not.toBe(val1);
+  expect(xsadd.getFloat()).not.toBe(val2);
+});
+
+it('should export a standalone random()', () => {
+  const xsadd = new XSadd(1234);
+  const value = xsadd.random();
+  expect(typeof value).toBe('number');
+  const random = xsadd.random;
+  random();
+  random();
+  expect(typeof random()).toBe('number');
+});
+
+it('should throw if seed is not an integer', () => {
+  const add = new XSadd();
+  [false, Infinity, NaN, '1', 0.2, 4.5].forEach((value: any) => {
+    expect(() => new XSadd(value)).toThrow(/seed must be an integer/);
+    expect(() => add.init(value)).toThrow(/seed must be an integer/);
+  });
+});
---
+++ src/xsadd.ts
@@ -0,0 +1,89 @@
+const LOOP = 8;
+const FLOAT_MUL = 1 / 16777216;
+
+const sh1 = 15;
+const sh2 = 18;
+const sh3 = 11;
+
+function multiply_uint32(n: number, m: number) {
+  n >>>= 0;
+  m >>>= 0;
+  const nlo = n & 0xffff;
+  const nhi = n - nlo;
+  return (((nhi * m) >>> 0) + nlo * m) >>> 0;
+}
+
+export default class XSadd {
+  public random: () => number;
+
+  private state: Uint32Array;
+
+  constructor(seed = Date.now()) {
+    this.state = new Uint32Array(4);
+    this.init(seed);
+    this.random = this.getFloat.bind(this);
+  }
+
+  /**
+   * Returns a 32-bit integer r (0 <= r < 2^32)
+   */
+  public getUint32() {
+    this.nextState();
+    return (this.state[3] + this.state[2]) >>> 0;
+  }
+
+  /**
+   * Returns a floating point number r (0.0 <= r < 1.0)
+   */
+  public getFloat() {
+    return (this.getUint32() >>> 8) * FLOAT_MUL;
+  }
+
+  public init(seed: number) {
+    if (!Number.isInteger(seed)) {
+      throw new TypeError('seed must be an integer');
+    }
+    this.state[0] = seed;
+    this.state[1] = 0;
+    this.state[2] = 0;
+    this.state[3] = 0;
+    for (let i = 1; i < LOOP; i++) {
+      this.state[i & 3] ^=
+        (i +
+          multiply_uint32(
+            1812433253,
+            this.state[(i - 1) & 3] ^ ((this.state[(i - 1) & 3] >>> 30) >>> 0)
+          )) >>>
+        0;
+    }
+    this.periodCertification();
+    for (let i = 0; i < LOOP; i++) {
+      this.nextState();
+    }
+  }
+
+  private periodCertification() {
+    if (
+      this.state[0] === 0 &&
+      this.state[1] === 0 &&
+      this.state[2] === 0 &&
+      this.state[3] === 0
+    ) {
+      this.state[0] = 88; // X
+      this.state[1] = 83; // S
+      this.state[2] = 65; // A
+      this.state[3] = 68; // D
+    }
+  }
+
+  private nextState() {
+    let t = this.state[0];
+    t ^= t << sh1;
+    t ^= t >>> sh2;
+    t ^= this.state[3] << sh3;
+    this.state[0] = this.state[1];
+    this.state[1] = this.state[2];
+    this.state[2] = this.state[3];
+    this.state[3] = t;
+  }
+}
